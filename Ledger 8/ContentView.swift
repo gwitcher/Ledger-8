d401f6cfd282d443ae2f2212b55dcb11d758dc05
Gabe Witcher <gabewitcher@gmail.com>
2025-04-15 17:02:56 -0700

    Initial Commit

---
+++ Ledger 8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ledger 8
+//
+//  Created by Gabe Witcher on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ledger 8/Ledger_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Ledger_8App.swift
+//  Ledger 8
+//
+//  Created by Gabe Witcher on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Ledger_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
