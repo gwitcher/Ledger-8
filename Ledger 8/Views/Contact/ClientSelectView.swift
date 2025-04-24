ebaa2ac21f47f4dc6eace653e8158ba1b66aa6a0
Gabe Witcher <gabewitcher@gmail.com>
2025-04-24 16:41:39 -0700

    save/update client

---
+++ Ledger 8/Views/Contact/ClientSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  ClientSelectView.swift
+//  Ledger 8
+//
+//  Created by Gabe Witcher on 4/24/25.
+//
+
+import SwiftUI
+
+struct ClientSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClientSelectView()
+}
